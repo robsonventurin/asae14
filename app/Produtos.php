f142b0f2bd9d297a2d3f1649736d6de11948ec5f
Robson Venturin <robventurin@gmail.com>
2020-05-02 09:42:48 -0300

    Adicionado Soft Deletes

--- app/Produtos.php
+++ app/Produtos.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Produtos extends Model
 {
+    use SoftDeletes;
+    
     protected $table = "produtos";
     protected $primaryKey = "id";
 
